de098b6b18bc0580f7b9d27b893164e39af8b9ed
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-07-08 06:59:00 +1000

    feat(profile): add experience/education & profile delete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -74,7 +74,7 @@ router.get("/handle/:handle", (req, res) => {
 router.get("/all", (req, res) => {
   const errors = {};
   Profile.find()
-    .populate("user", ["name", "avatar"])
+    .populate("user", ["name", "email", "avatar"])
     .then(profiles => {
       if (!profiles) {
         errors.noprofile = "There are no profiles";
@@ -269,6 +269,21 @@ router.delete(
         profile.save().then(profile => res.json(profile));
       })
       .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
   }
 );
 
