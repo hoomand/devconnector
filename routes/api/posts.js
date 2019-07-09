4556333f97d290f7798acc4e2e7ef80cc6ae73de
Bijan 🎧📚🎮 Hoomand <hoomandb@amazon.com>
2019-07-10 09:07:16 +1000

    feat(comment): add post comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -157,4 +157,30 @@ router.delete(
   }
 );
 
+// @route   POST api/post/comment/:id
+// @desc    Add a comment
+// @access  Private
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        // Add to comments array
+        post.comments.unshift(newComment);
+
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "Post not found" }));
+  }
+);
+
 module.exports = router;
