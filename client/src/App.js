25831bd05af5f5c07817431710979af94f97b4d7
Bijan 🎧📚🎮 Hoomand <hoomandb@amazon.com>
2019-07-17 18:06:18 +1000

    feat(Navbar): add navbar with router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <h1>My React App</h1>
+      </div>
+    </Router>
   );
 }
 
