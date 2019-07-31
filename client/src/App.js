5623a30264bbc2717afbf1fff504606d06226edc
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-07-31 11:03:25 +1000

    feat: add CreateProfile

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import { clearCurrentProfile } from "./actions/profileActions";
 import PrivateRoute from "./components/common/PrivateRoute";
+import CreateProfile from "./components/Profile/CreateProfile";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -45,6 +46,13 @@ function App() {
             <Route exact path="/login" component={Login} />
             <Switch>
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
+            <Switch>
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </div>
           <Footer />
